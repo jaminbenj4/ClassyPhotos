dfb229b1cc9a4722d1d3936862a192c7b58b3dd8
jaminbenjones <benj.jones4@gmail.com>
2017-06-02 23:00:37 -0700

    added console app for testing

---
+++ ClasseyPhotos/TestConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.System);
+            Console.WriteLine(path);
+            Console.ReadLine();
+        }
+    }
+}
