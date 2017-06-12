68022919c44cc8df36cc4eed9f7564c2847e0dec
Hunter Carlson <carlson.hunter@gmail.com>
2017-06-12 10:39:51 -0700

    split wallpaper functionality into class library

---
+++ ClasseyPhotos/WallpaperTools/RegKey.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace WallpaperTools
+{
+    public class RegKey
+    {
+        public RegKey()
+        {
+            Values = new Dictionary<string, string>();
+        }
+
+        public Dictionary<string, string> Values { get; }
+    }
+}
